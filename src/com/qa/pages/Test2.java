25c8052114141a6d727f42e9ef7e93762ba5497b
pradeep0410 <pradeep04.ibm@gmail.com>
2025-09-20 18:23:08 +0530

    commit test2.java to prad_branch

---
+++ src/com/qa/pages/Test2.java
@@ -0,0 +1,18 @@
+package com.qa.pages;
+
+public class Test2 {
+
+	public static void main(String[] args) {
+
+		String hungry = "yes";
+		
+		if(hungry=="yes") {
+			System.out.println("eat something....");
+			
+		}else {
+			System.out.println("do work....");
+		}
+
+	}
+
+}
